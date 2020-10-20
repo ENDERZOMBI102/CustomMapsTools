ff8de59d224ef4601755b4107603676fc083af53
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-10-20 20:01:24 +0200

    renamed config

--- src/main/java/com/enderzombi102/cmt/config/ModConfig.java
+++ src/main/java/com/enderzombi102/cmt/config/ModConfig.java
@@ -4,7 +4,7 @@ import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
 import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
 
-@Config(name = "modid")
+@Config(name = "Custom Maps Tools")
 public class ModConfig implements ConfigData {
 	boolean toggleA = true;
 	boolean toggleB = false;
