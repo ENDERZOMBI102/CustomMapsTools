7b093a9e05a3146347c20c89091d2f9695e81f3d
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2020-12-26 18:28:09 +0100

    optimized imports removed keybinding test
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/main/java/com/enderzombi102/cmt/CustomMapsTools.java
+++ src/main/java/com/enderzombi102/cmt/CustomMapsTools.java
@@ -1,9 +1,6 @@
 package com.enderzombi102.cmt;
 
-import com.enderzombi102.cmt.client.Key;
-import com.enderzombi102.cmt.client.KeyBindingHelper;
 import com.enderzombi102.cmt.config.ModConfig;
-import com.enderzombi102.cmt.gamerule.GameruleVisitor;
 import com.enderzombi102.cmt.gamerule.TextGamerule;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
 import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
@@ -11,18 +8,18 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.GameRules;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /** TODO: MinecraftClient.openScreen() calls updateTitle that gets the title from getWindowTitle
-*	need to mixin in it and do an early return to give it MY title
+ *	need to mixin in it and do an early return to give it MY title
  */
 
-
-
+// ---- this code gets the content of a gamerule ----
+// MinecraftServer server = client.getServer();
+// GameRules.Rule<?> rule = server.getGameRules().get(key);
+// logger.info( rule.toString() );
 
 public class CustomMapsTools implements ModInitializer, ClientModInitializer, DedicatedServerModInitializer {
 
@@ -40,20 +37,6 @@ public class CustomMapsTools implements ModInitializer, ClientModInitializer, De
 	@SuppressWarnings("unchecked")
 	public void onInitializeClient() {
 		logger.info("Setting up window callbacks");
-		// until its fixed, disable this "module"
-		KeyBindingHelper.makeKeybind(
-				Key.GLFW_KEY_K,
-				client -> {
-					MinecraftServer server = client.getServer();
-					GameRules.Rule<?> rule = server.getGameRules().get(key);
-					logger.info( rule.toString() );
-				},
-				"text.invalid.translation",
-				"key.categories.movement",
-				false,
-				false
-		);
-
 	}
 
 	@Override
