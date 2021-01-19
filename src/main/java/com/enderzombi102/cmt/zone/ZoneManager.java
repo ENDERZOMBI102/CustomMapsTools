4cd6341e88382d2c093d6a1649d1d3e9cb4f0fa1
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-01-19 11:36:50 +0100

    finished ZoneManager
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

---
+++ src/main/java/com/enderzombi102/cmt/zone/ZoneManager.java
@@ -0,0 +1,94 @@
+package com.enderzombi102.cmt.zone;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("UnstableApiUsage")
+public class ZoneManager implements ZoneComponent {
+
+	private final ServerWorld world;
+	private final ArrayList< AbstractZone<? extends Entity> > zones = new ArrayList<>();
+
+	public ZoneManager(World world) {
+		if (world instanceof ServerWorld) this.world = (ServerWorld) world;
+		else this.world = null;
+	}
+
+	@Override
+	public void writeToNbt(CompoundTag tag) {
+		ListTag list = new ListTag();
+
+		for (int i = 0; i < this.zones.size(); i++ ) {
+			if ( this.zones.get(i).deleteOnExit ) continue;
+			CompoundTag compoundTag = new CompoundTag();
+			this.zones.get(i).writeToNbt(compoundTag);
+			list.addTag(i, compoundTag);
+		}
+
+		tag.put("zones", list);
+	}
+
+	@Override
+	public void readFromNbt(CompoundTag tag) {
+		this.zones.clear();
+		for ( Tag ctag : ( (ListTag) tag.get("zones") ).toArray( new Tag[0] ) ) {
+
+			CompoundTag compoundTag = (CompoundTag) ctag;
+			AbstractZone<? extends Entity> zone;
+
+			switch ( compoundTag.getString("type") ) {
+				case "FunctionZone":
+					zone = new FunctionZone(this.world, compoundTag);
+					break;
+				default:  // this includes the "CommandZone" class/zone
+					zone = new CommandZone(this.world, compoundTag);
+			}
+
+			this.zones.add( zone );
+		}
+	}
+
+	@Override
+	public void serverTick() {
+		for ( AbstractZone<? extends Entity> zone : this.zones) {
+			zone.tick();
+		}
+	}
+
+	@Override
+	public void addZone(AbstractZone<? extends Entity> zone) {
+		zone.world = this.world;
+		if (this.world != null) this.zones.add(zone);
+	}
+
+	@Override
+	public void removeZone(PlayerEntity entity) {
+		for (AbstractZone<? extends Entity> zone : this.zones) {
+			if( zone.getBox().intersects( entity.getBoundingBox() ) ) {
+				this.zones.remove( zone );
+				return;
+			}
+		}
+	}
+
+	@Override
+	public boolean containZoneWithIdentifier(String id) {
+		for (AbstractZone<? extends Entity> zone : this.zones) {
+			if ( zone.id.equals(id) ) return true;
+		}
+		return false;
+	}
+
+	public List< AbstractZone<? extends Entity> > getZones() {
+		return this.zones;
+	}
+
+}
