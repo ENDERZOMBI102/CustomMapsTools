81a1b1c11ac7dc2fe834646b4d366fb22357a226
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-01-25 17:23:11 +0100

    ordered added more methods removed rendundant methods
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/main/java/com/enderzombi102/cmt/zone/ZoneComponent.java
+++ src/main/java/com/enderzombi102/cmt/zone/ZoneComponent.java
@@ -4,6 +4,7 @@ import dev.onyxstudios.cca.api.v3.component.ComponentV3;
 import dev.onyxstudios.cca.api.v3.component.tick.ServerTickingComponent;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -13,9 +14,10 @@ public interface ZoneComponent extends ComponentV3, ServerTickingComponent {
 	void addZone(AbstractZone<? extends Entity> zone);
 	String removeZone(PlayerEntity entity);
 	void removeZone(String id);
-	boolean containZoneWithIdentifier(String id);
-	boolean playerInZone(List<PlayerEntity> entities, String id);
+	boolean containsZone(String id);
 	boolean entityInZone(List<Entity> entities, String id);
-	AbstractZone<? extends Entity> getZone(String id);
+	@Nullable AbstractZone<? extends Entity> getZone(String id);
 	List< AbstractZone<? extends Entity> > getZones();
+	boolean isIdValid(String id);
+	String getRandomId();
 }
