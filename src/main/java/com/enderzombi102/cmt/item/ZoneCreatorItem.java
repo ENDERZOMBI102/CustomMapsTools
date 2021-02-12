86c717feacb48967ac5bed4568fb9d5ee90df0f5
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-02-12 19:03:11 +0100

    this can now create zones
    Signed-off-by: ENDERZOMBI102 <enderzombi102.end@gmail.com>

--- src/main/java/com/enderzombi102/cmt/item/ZoneCreatorItem.java
+++ src/main/java/com/enderzombi102/cmt/item/ZoneCreatorItem.java
@@ -7,6 +7,7 @@ import com.enderzombi102.cmt.zone.*;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
@@ -14,6 +15,8 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
@@ -33,26 +36,32 @@ public class ZoneCreatorItem extends Item {
 
 	@Override
 	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext ctx) {
+		if ( world == null ) return;
 		tooltip.add( new LiteralText("Zone type: " + ZoneTypes.getType(this.type).toString() ) );
-		tooltip.add( new LiteralText("Position 0: " + this.pos0.toShortString() ) );
-		tooltip.add( new LiteralText("Position 1: " + this.pos1.toShortString() ) );
+		tooltip.add( new LiteralText("Position 0: " + ( this.pos0 != null ? this.pos0.toShortString() : "not set") ) );
+		tooltip.add( new LiteralText("Position 1: " + ( this.pos1 != null ? this.pos1.toShortString() : "not set") ) );
+	}
+
+	@Override
+	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+		if ( world.isClient() ) return TypedActionResult.pass( user.getStackInHand(hand) );
+		if ( user.isSneaking() ) {
+			if ( type == ZoneTypes.lenght - 1) type = 0;
+			else type++;
+			user.sendMessage( new LiteralText("Zone type set to {}.".replace("{}", ZoneTypes.getType(this.type).toString() ) ), true);
+		}
+		return TypedActionResult.pass( user.getStackInHand(hand) );
 	}
 
 	@Override
 	public ActionResult useOnBlock(ItemUsageContext ctx) {
 		if ( ctx.getPlayer() == null || ctx.getWorld().isClient() ) return ActionResult.PASS;
-		if ( ctx.getPlayer().isSneaking() ) {
-			if ( type == ZoneTypes.lenght - 1) type = 0;
-			else type++;
-			ctx.getPlayer().sendMessage( new LiteralText("Zone type set to {}.".replace("{}", ZoneTypes.getType(this.type).toString() ) ), true);
+		if ( pos0 == null ) {
+			this.pos0 = ctx.getBlockPos();
+			ctx.getPlayer().sendMessage( new LiteralText("Point 0 set to {}.".replace("{}", this.pos0.toShortString() ) ), true);
 		} else {
-			if ( pos0 == null ) {
-				this.pos0 = ctx.getBlockPos();
-				ctx.getPlayer().sendMessage( new LiteralText("Point 0 set to {}.".replace("{}", this.pos0.toShortString() ) ), true);
-			} else {
-				this.pos1 = ctx.getBlockPos();
-				ctx.getPlayer().sendMessage( new LiteralText("Point 1 set to {}.".replace("{}", this.pos1.toShortString() ) ), true);
-			}
+			this.pos1 = ctx.getBlockPos();
+			ctx.getPlayer().sendMessage( new LiteralText("Point 1 set to {}.".replace("{}", this.pos1.toShortString() ) ), true);
 		}
 		return ActionResult.SUCCESS;
 	}
